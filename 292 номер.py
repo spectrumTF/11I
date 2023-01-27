630d652003dd98fcd366d8b0f38ceec123db3346
Alexsander-MEGA-MOZG <123737033+Alexsander-MEGA-MOZG@users.noreply.github.com>
2023-01-27 14:40:01 +0300

    Add files via upload

---
+++ 292 номер.py
@@ -0,0 +1,10 @@
+for i in range(1000):
+    n=bin(i)[2:]
+    if n.count('1')%2==0:
+        n = '10' + n[2:] +'0'
+    else:
+        n='11'+n[2:] +'1'
+    r=int(n,2)
+    if r<=35:
+        print(i)
+    
