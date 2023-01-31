50a72c2449d7449da8f113c74834e92a385da7af
anastasionnnn <124043306+anastasionnnn@users.noreply.github.com>
2023-01-31 19:23:33 +0300

    Add files via upload

---
+++ papki/anastasion/5/272.py
@@ -0,0 +1,15 @@
+for i in range(1,1000):
+    n=i
+    a=''
+    while n>0:
+        a=str(n%4)+a
+        n=n//4
+    if i%2!=0:
+        a='2'+a+'11'
+    else:
+        a='13'+a+'02'
+        r=int(a,4)
+        if r>1000:
+            print(r)
+            break
+        
