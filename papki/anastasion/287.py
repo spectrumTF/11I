72eba2d1b9115c7c93138561633b0409fa2aabc5
anastasionnnn <124043306+anastasionnnn@users.noreply.github.com>
2023-01-31 13:53:24 +0300

    Add files via upload

---
+++ papki/anastasion/287.py
@@ -0,0 +1,11 @@
+c=0
+for i in range(1000):
+    n=bin(i) [2:]
+    if i%2==0:
+        n='1'+n+'11'
+    else:
+        n='11'+n+'0'
+    r=int(n,2)
+    if 500<=r<=1000:
+        c+=1
+        print(c)
