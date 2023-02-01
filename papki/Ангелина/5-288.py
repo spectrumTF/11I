a462825e8c81cde87e3f337080499f182beafd64
Neangelina <124124834+Neangelina@users.noreply.github.com>
2023-02-01 11:13:03 +0300

    Add files via upload

---
+++ papki/Ангелина/5-288.py
@@ -0,0 +1,11 @@
+a = []
+for i in range(10000):
+    n = bin(i)[2:]
+    if i % 2 == 0:
+        n = '1' + n + '10' 
+    else:
+        n = '11' + n + '0' 
+    r = int(n, 2)
+    if 800<=r<=1500:
+        a.append(r)
+print(len(set(a)))
