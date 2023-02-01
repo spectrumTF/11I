289782fa10cf77a0ecafb0131e3bda4665f4c708
ioopentx <123736794+ioopentx@users.noreply.github.com>
2023-02-01 11:01:02 +0300

    Create 291.py

---
+++ papki/Agalakov/5ege/291.py
@@ -0,0 +1,12 @@
+for i in range(10000):
+    n = bin(i)[2:]
+    if n.count('1')%2==0:
+        n = '1' + n[2:] + '0'
+    else:
+        n = '11' + n[2:] + '1'
+    r = int(n,2)
+    if r>15:
+        print(i)
+        break
+
+        
