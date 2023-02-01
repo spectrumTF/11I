6f0f4b2cc313c780672483dd8c6bc4a8db7c4491
Atranus <80592133+Atranus@users.noreply.github.com>
2023-02-01 10:38:34 +0300

    Add files via upload

---
+++ papki/Андрей_Кладов/№5/#5ege305.py
@@ -0,0 +1,24 @@
+for n in range(10000000):
+    x=hex(n)[2:]
+    if n%2==0:
+        x=x+'F'
+    else:
+        x=x+'0'
+    x1=x
+    f=0
+    while x1!='':
+        f+=int(x1[-1:],16)
+        x1=x1[:-1]
+    x=x+hex(f%16)[2:]
+    x1=x
+    f=0
+    while x1!='':
+        f+=int(x1[-1:],16)
+        x1=x1[:-1]
+    x=x+hex(f%16)[2:]
+    r=int(x,16)
+    ma=max(x)
+    mi=min(x)
+    if x.count(ma)/5==x.count(mi):
+        print(n)
+        break
