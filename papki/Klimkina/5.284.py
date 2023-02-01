f14ae996c911b7d0aea1a02cdc990f63545163c2
AmityOhCramity <95761424+cortybloodyfox@users.noreply.github.com>
2023-02-01 10:32:29 +0300

    Add files via upload

---
+++ papki/Klimkina/5.284.py
@@ -0,0 +1,12 @@
+c=0
+for i in range(0,1000):
+    n=bin(i)[2:]
+    if i%2==0:
+        s=n.count('1')
+        n=n+bin(s)[2:]
+    else:
+        n='1'+n+'00'
+    r=int(n,2)
+    if 500<=r<=700:
+        c+=1
+        print(c)
