59da45f59943f892a94eedfe9276d2d6eb1e8016
20ballovEGE <124138369+20ballovEGE@users.noreply.github.com>
2023-02-01 19:12:22 +0300

    Delete papki/Eugene Afanasev/5ege directory

--- papki/Eugene Afanasev/5ege/279.py
+++
@@ -1,9 +0,0 @@
-def f(x):
-    h=list(map(int,str(x)))
-    a=sum(d for d in h if d%2!=0)
-    b=sum(h[::-2])
-    return abs(a-b)
-for x in range(100000000):
-    if f(x)==29:
-        print(x)
-        break
