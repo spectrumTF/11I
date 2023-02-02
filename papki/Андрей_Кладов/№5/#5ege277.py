f9aeea0044f53ffc4f642d0f3161e0767c17919d
Atranus <80592133+Atranus@users.noreply.github.com>
2023-02-02 09:16:17 +0300

    Add files via upload

---
+++ papki/Андрей_Кладов/№5/#5ege277.py
@@ -0,0 +1,16 @@
+for n in range(1,10**8):
+    x=str(n)
+    S1=0
+    S2=0
+    for i in range(len(x)):
+        if int(x[i])%2!=0:
+            S1+=int(x[i])
+        if (i+1)%2==0:
+            S2 += int(x[i])
+    if (S1-S2)<0:
+        r=S2-S1
+    else:
+        r=S1-S2
+    if r==30:
+        print(n)
+        break
