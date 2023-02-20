f8d50a57539240f6203c24ed8c27f8c803133bd2
anastasionnnn <124043306+anastasionnnn@users.noreply.github.com>
2023-02-20 17:20:09 +0300

    Add files via upload

---
+++ papki/anastasion/17/194.py
@@ -0,0 +1,7 @@
+s=[int(x) for x in open('17-9.txt')]
+b=[bin(x)[2:] for x in s]
+a=[]
+for i in range(len(s)-2):
+    if (int(b[i].count('1')==2 and b[i].count('0')>=1)+int(b[i+1].count('1')==2 and b[i+1].count('0')>=1)+int(b[i+2].count('1')==2 and b[i+2].count('0')>=1))>=2:
+        a.append(max(s[i],s[i+1],s[i+2]))
+print(len(a),sum(a))
