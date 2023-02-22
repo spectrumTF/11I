704cf9edfbf45ac54f88a6a73b2e9ab75d018d48
AlinaSemenovaa <95761541+AlinaSemenovaa@users.noreply.github.com>
2023-02-22 23:12:13 +0300

    Delete 180.py

--- papki/Alina/17 номер/180.py
+++
@@ -1,8 +0,0 @@
-with open('17-4.txt') as f:
-    a=[int(x) for x in f.readlines()]
-a1=[]
-for i in range(len(a)):
-    #if (a[i]%10==5 or a[i]%10==7) and a[i]%9!=0
-    if a[i]%5==3 and a[i]%9==5 and a[i]%8!=7:
-        a1.append(a[i])
-print(len(a1),max(a1))        
