9aaaebe05f5bfd6bd4079efa8f25e45491f8b297
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:23:00 +0300

    Delete 205.py

--- papki/Andrey Kurochkin/205.py
+++
@@ -1,8 +0,0 @@
-a = [int(x) for x in open("17-205.txt")]
-count=0
-c = []
-for i in range(len(a)-1):
-    if abs(a[i] - a[i+1])%2==0 and abs(a[i] - a[i+1])%37==0 :
-        count+=1
-        c.append(a[i]+a[i+1])
-print(count,max(c))
