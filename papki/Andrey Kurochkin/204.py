bf31b8796472c2a17db0af1115c5b08a41e01b88
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:22:55 +0300

    Delete 204.py

--- papki/Andrey Kurochkin/204.py
+++
@@ -1,8 +0,0 @@
-a = [int(x) for x in open("17-204.txt")]
-c = []
-count=0
-for i in range(len(a)-2):
-    if not(a[i]>0 and a[i]%10==9) and (a[i+1]>0 and a[i+1]%10==9) and not(a[i+2]>0 and a[i+2]%10==9):
-        count+=1
-        c.append(a[i]+a[i+1]+a[i+2])
-print(count,max(c))
