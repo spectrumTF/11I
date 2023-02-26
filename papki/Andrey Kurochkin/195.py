6ca0a3149ce6ad195bc5156632b47d72d44dab54
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:21:25 +0300

    Delete 195.py

--- papki/Andrey Kurochkin/195.py
+++
@@ -1,7 +0,0 @@
-a=[int(x) for x in open('17-9.txt')]
-b=[bin(x)[2:] for x in a]
-c=[]
-for i in range(len(a)-2):
-    if (b[i].count('1')>=3 and b[i+1].count('1')>=3 and b[i].count('0')>=1 and b[i+1].count('0')>=1) or (b[i].count('1')>=3 and b[i+2].count('1')>=3 and b[i].count('0')>=1 and b[i+2].count('0')>=1) or (b[i+1].count('1')>=3 and b[i+2].count('1')>=3 and b[i+1].count('0')>=1 and b[i+2].count('0')>=1):
-        c.append(max(a[i],a[i+1],a[i+2]))
-        print(len(c),max(c))
