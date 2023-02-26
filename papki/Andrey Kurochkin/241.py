f7fbe95435d9081de01b1eef21bce8f1b58f119c
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:27:12 +0300

    Delete 241.py

--- papki/Andrey Kurochkin/241.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in open("17-1.txt")]
-c=[]
-s=sum(a)/len(a)
-for i in range(len(a)-2):
-    if ((a[i]<s and a[i+1]<s) or (a[i+2]<s and a[i+1]<s) or (a[i]<s and a[i+2]<s)) and ((str(a[i]).count("5")>=1 and str(a[i+1]).count("5")>=1) or (str(a[i+2]).count("5")>=1 and str(a[i]).count("5")>=1) or (str(a[i+1]).count("5")>=1 and str(a[i+2]).count("5")>=1)) :
-        c.append((a[i]+a[i+1]+a[i+2]))
-print(len(c),max(c))
