afb1863f1a86a259971b7f0b26d3858c3975d21e
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:23:11 +0300

    Delete 207.py

--- papki/Andrey Kurochkin/207.py
+++
@@ -1,8 +0,0 @@
-a = [int(x) for x in open("17-205.txt")]
-count=0
-c = []
-for i in range(len(a)-1):
-    if( abs(a[i])%10==7 or abs(a[i+1])%10==7 ) and abs(a[i]+a[i+1])%12==0:
-        count+=1
-        c.append(a[i]+a[i+1])
-print(count,max(c))
