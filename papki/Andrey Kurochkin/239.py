94eb4589a3a8d859e2377d203509a5a3308f2c9f
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:26:54 +0300

    Delete 239.py

--- papki/Andrey Kurochkin/239.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in open("17-1.txt")]
-c=[]
-s=sum(a)/len(a)
-for i in range(len(a)-2):
-    if ((a[i]<s and a[i+1]<s) or (a[i+2]<s and a[i+1]<s) or (a[i]<s and a[i+2]<s)) and ((abs(a[i])%10==8 and abs(a[i+1])%10==8) or (abs(a[i+1])%10==8 and abs(a[i+2])%10==8)or (abs(a[i])%10==8 and abs(a[i+2])%10==8)) :
-        c.append((a[i]+a[i+1]+a[i+2]))
-print(len(c),max(c))
