0e0466ce4ab99c90d366122b08163521f177f5d8
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:25:40 +0300

    Delete 227.py

--- papki/Andrey Kurochkin/227.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in open("17-1.txt")]
-c = []
-s = sum(a)/len(a)
-for i in range(len(a)-2):
-    if (a[i]>s or a[i+1]>s or a[i+2]>s):
-        c.append(a[i]+a[i+1]+a[i+2])
-print(len(c),max(c))
