9d83dd7d24f6d2ceffcf5f0810637dfca1bd4e6d
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:28:00 +0300

    Delete 257.py

--- papki/Andrey Kurochkin/257.py
+++
@@ -1,9 +0,0 @@
-a = [int(x) for x in open("17-257.txt")]
-b = [int(x) for x in a if x%7==0]
-c = [int(x) for x in a if x%13==0]
-z = min(b)
-y = min(c)
-if z > y:
-    print(len(b),max(b))
-else:
-    print(len(c),max(c))
