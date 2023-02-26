60d8871a3b719f9c1d2e6316d9f0702f65b63cae
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:27:07 +0300

    Delete 246.py

--- papki/Andrey Kurochkin/246.py
+++
@@ -1,8 +0,0 @@
-a = [int(x) for x in open("17-243.txt")]
-b = [z for z in a if z%171==0]
-c = []
-s = max(b)
-for i in range(len(a)-1):
-    if (a[i]<s or a[i+1]<s) and (a[i]%2!=0 or a[i+1]%2!=0):
-        c.append(a[i]+a[i+1])
-print(len(c),max(c))
