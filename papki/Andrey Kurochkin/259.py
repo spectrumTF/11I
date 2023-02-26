fdfa2ce8749b680002f0aec4c7dd235d5c50ed48
AnnDreydgf <95761814+AnnDreydgf@users.noreply.github.com>
2023-02-27 00:28:09 +0300

    Delete 259.py

--- papki/Andrey Kurochkin/259.py
+++
@@ -1,7 +0,0 @@
-a = [int(x) for x in open("17-257.txt")]
-b = [int(x) for x in a if x%11==0]
-c = [int(x) for x in a if x%17==0]
-if len(b) > len(c):
-    print(len(b),min(b))
-else:
-    print(len(c),max(c))
