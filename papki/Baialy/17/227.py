d35d7603365ee1fb70f73c284e7a1e3becd2d90b
SherovBaialy <123735674+SherovBaialy@users.noreply.github.com>
2023-03-01 00:37:29 +0300

    Create 227.py

---
+++ papki/Baialy/17/227.py
@@ -0,0 +1,8 @@
+with open('17-1.txt') as f:
+    a=[int(x) for x in f.readlines()]
+a1=[]
+sr=sum(a)/len(a)
+for i in range(len(a)-2):     
+    if (a[i]>sr or a[i+1]>sr or a[i+2]>sr):
+        a1.append(a[i]+a[i+1]+a[i+2])
+print(len(a1),max(a1))
