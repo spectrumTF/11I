7df2b9ab3a170f2ddaf0c59f583f97f1c3625c23
SherovBaialy <123735674+SherovBaialy@users.noreply.github.com>
2023-03-01 00:27:05 +0300

    Create 200.py

---
+++ papki/Baialy/17/200.py
@@ -0,0 +1,7 @@
+with open('17-199.txt') as f:
+    a=[int(x) for x in f.readlines()]
+a1=[]
+for i in range(len(a)-2):  
+    if a[i+1] in range(10,100) and a[i+1]%2!=0 and (not (a[i] in range(10,100)) or a[i]%2==0) and (not (a[i+2] in range(10,100)) or a[i+2]%2==0): 
+        a1.append(a[i]+a[i+1]+a[i+2])
+print(len(a1),max(a1))
