f655a2ddf6f2bd6dfb0066deb42dfff08a7d4329
SherovBaialy <123735674+SherovBaialy@users.noreply.github.com>
2023-03-01 00:35:31 +0300

    Create 224.py

---
+++ papki/Baialy/17/224.py
@@ -0,0 +1,8 @@
+with open('17-4.txt') as f:
+    a=[int(x) for x in f.readlines()]
+a1=[]
+sr=sum(a)/len(a)
+for i in range(len(a)-1):     
+    if (a[i]<sr or a[i+1]<sr) and (not ('5' in str(a[i])) or not ('5' in str(a[i+1]))) :
+        a1.append(a[i]+a[i+1])
+print(len(a1),min(a1))
